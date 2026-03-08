1ff903f5f98bac858d57e3274b42df8c250c35ee
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:53:29 +0500

    Create ChatMediator.java

---
+++ src/mediator/ChatMediator.java
@@ -0,0 +1,23 @@
+package mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatMediator implements IMediator {
+
+    private List<User> users = new ArrayList<>();
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public void sendMessage(String message, User sender){
+
+        for(User user : users){
+
+            if(user != sender){
+                user.receive(message);
+            }
+        }
+    }
+}
