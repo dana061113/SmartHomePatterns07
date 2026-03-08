d61216b606214e0f61cf17a9e585b92a60d1f689
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:51:54 +0500

    Create LightOffCommand.java

---
+++ src/command/LightOffCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class LightOffCommand implements ICommand {
+
+    private Light light;
+
+    public LightOffCommand(Light light){
+        this.light = light;
+    }
+
+    public void execute(){
+        light.off();
+    }
+}
