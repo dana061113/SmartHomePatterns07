84159c2fbc890108fb2d45a4ad15e749210e5033
dana061113 <danakobylandykyzy@gmail.com>
2026-03-08 21:36:21 +0500

    Create Light.java

---
+++ src/command/Light.java
@@ -0,0 +1,12 @@
+package command;
+
+public class Light {
+
+    public void on(){
+        System.out.println("Light ON");
+    }
+
+    public void off(){
+        System.out.println("Light OFF");
+    }
+}
